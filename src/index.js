83eb83768e68361707d4051fd76d7ec1255285f3
adahcodxd <amritanshujha96@gmail.com>
2024-06-22 16:01:54 +0530

    add existing file

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+dotenv.config({
+  path: './.env'
+});
+
+// Now you can use connectDB or any other code that depends on environment variables
+
+/*import express from  "express";
+const app=express()
+;(async()=>{
+    try {
+        mongoose.connect('$(process.env.MONGODB_URI}/${DB_NAME}')
+        app.on("error",()=>{
+            console.log("ERR",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log('app is running on port ${process.env.PORT}');
+        }
+        )
+    } catch (error) {
+        console.error("ERROR:",error)
+        throw err
+    }
+})()
+*/
